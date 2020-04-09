b09e1ee9410b5a1ec35e50f8f02992d967e1b7de
Ankit_Goyal <goyala960@gmail.com>
2020-04-10 02:49:07 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,32 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("new user connected");
 
+    socket.emit("newMessage",{
+        from : "admin",
+        text : "welcome to chat app",
+        createAt : new Date().getTime(),
+    });
+
+    socket.broadcast.emit("newMessage",{
+        from : "admin",
+        text : "new user joined",
+        createAt : new Date().getTime(),
+    });
+    
     socket.on("createMessage", (message) => {
         console.log("createMessage", message);
-        io.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createAt: new Date().getTime(),
-        });
+
+        // io.emit("newMessage", {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime(),
+        // });
+
+        // socket.broadcast.emit("newMessage", {
+        //         from: message.from,
+        //         text: message.text,
+        //         createAt: new Date().getTime()
+        // })
     });
 
     socket.on("disconnect", () => {
