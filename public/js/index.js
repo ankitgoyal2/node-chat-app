2437828bb0863a50937ff625609c60113f9546e0
Ankit_Goyal <goyala960@gmail.com>
2020-04-10 15:10:37 +0530

    add form for input and show messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,30 @@ socket.on("connect", () => {
 
 socket.on("newMessage", (newMessage) => {
     console.log("newMessage", newMessage);
+
+    var li = jQuery("<li></li>");
+    li.text(`${newMessage.from} : ${newMessage.text}`);
+    jQuery("#messages").append(li);
 });
+
+// socket.emit("createMessage",{
+//     from :"akash",
+//     text : "kurknrelilm"
+// },(acknoledgement)=>{
+//     console.log("got it -->",acknoledgement);
+// })
 
 socket.on("disconnect", () => {
     console.log("disconnected from server");
 });
+
+jQuery("#message-form").on("submit",(e)=>{
+    e.preventDefault();
+
+    socket.emit("createMessage",{
+        from : "User",
+        text : jQuery("[name = message]").val()
+    },(message)=>{
+        console.log(message);
+    })
+})
--- server/server.js
+++ server/server.js
@@ -21,10 +21,12 @@ io.on("connection", (socket) => {
 
     socket.broadcast.emit("newMessage",generateMessage( "admin", "new user joined"));
     
-    socket.on("createMessage", (message) => {
+    socket.on("createMessage", (message,callback) => {
         console.log("createMessage", message);
 
         io.emit("newMessage", generateMessage( message.from, message.text));
+
+        callback("this is from server");
 
         // socket.broadcast.emit("newMessage", {
         //         from: message.from,
